3e181730376f48744586bbd952301b65ffaf859e
diegovanne <dinhvanle.it@gmail.com>
2022-04-24 16:23:04 +0700

    Parent to child communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,14 @@ export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(res => {
-      this.users = res;
-      console.log(this.users);
-    }, err => {
-      console.log(err);
-    })
   }
 }
